51c5c09704f2e26e9ad75b3d7c74f32ef343c12f
unknown <purple.mv@gmail.com>
2019-08-19 17:26:47 -0500

    Variables: strings

---
+++ clase1.js
@@ -0,0 +1,15 @@
+var nombre = 'PurpleDoll', apellido = 'Dollanganger';
+
+var nombreEnMayusculas = nombre.toUpperCase();
+var apellidoEnMinusculas = apellido.toLowerCase();
+
+var primeraLetraDelNombre = nombre.charAt(0);
+var cantidadDeLetrasDelNombre = nombre.length;
+
+var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`;
+
+var str = nombre.substr(1, 2);
+
+var ultima = nombre.charAt(nombre.length-1);
+
+console.log(ultima);
