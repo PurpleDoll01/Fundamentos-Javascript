6c00712d259d67c017ad36ee8d625c98ede160b6
Mariana Valencia <purple.mv@gmail.com>
2019-09-23 17:41:36 -0500

    For

--- clase1.js
+++ clase1.js
@@ -2,53 +2,26 @@ var purpledoll = {
   nombre: 'Mariana',
   apellido: 'Valencia',
   edad: 29,
-  ingeniero: false,
-  cocinero: false,
-  cantante: false,
-  dj: false,
-  guitarrista: false,
-  drone: true
+  peso: 52
 }
 
-var juan = {
-  nombre: 'Juan',
-  apellido: 'Gomez',
-  edad: 13
+console.log(`Al inicio del año ${purpledoll.nombre} pesa ${purpledoll.peso}kg`);
+
+const INCREMENTO_PESO = 0.2;
+const DIAS_DEL_ANO = 365;
+
+const aumentarDePeso = persona => persona.peso += INCREMENTO_PESO;
+const adelgazar = persona => persona.peso -= INCREMENTO_PESO;
+
+
+for (var i=1; i <= DIAS_DEL_ANO; i++) {
+  var random = Math.random();
+
+  if (random < 0.25) {
+    aumentarDePeso(purpledoll);
+  } else if (random < 0.5) {
+    adelgazar(purpledoll);
+  } 
 }
 
-function imprimirProfesiones(persona) {
-  console.log(`${persona.nombre} es:`);
-
-  if (persona.ingeniero) {
-    console.log('Ingeniero');
-  } else {
-    console.log('No es ingeniero');
-  }
-  if (persona.cocinero) {
-    console.log('Cocinero');
-  }
-  if (persona.drone) {
-    console.log('Vuela drone');
-  }
-}
-
-const MAYORIA_DE_EDAD = 18;
-
-const esMayorDeEdad = ({ edad }) => edad >= MAYORIA_DE_EDAD;
-
-function imprimirSiEsMayorDeEdad(persona) {
-  if (esMayorDeEdad(persona)) {
-    console.log(`${persona.nombre} es mayor de edad`);
-  } else {
-    console.log(`${persona.nombre} es menor de edad`);
-  }
-}
-
-
-const permitirAcceso = ({ edad }) => {
-  if (!esMayorDeEdad({ edad })) {
-    console.log('ACCESO DENEGADO');
-  } else {
-    console.log('ENtre ps');
-  }
-}
+console.log(`Al final del año ${purpledoll.nombre} pesa ${purpledoll.peso.toFixed(1)}kg`);
