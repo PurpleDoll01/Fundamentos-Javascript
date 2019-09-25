0750f442797d79da290ab98d877e3e45d093fe97
Mariana Valencia <purple.mv@gmail.com>
2019-09-24 20:09:36 -0500

    While

--- clase1.js
+++ clase1.js
@@ -1,31 +1,14 @@
-var purpledoll = {
-  nombre: 'Mariana',
-  apellido: 'Valencia',
-  edad: 29,
-  peso: 52
+var contador = 0;
+
+const llueve = () => Math.random() < 0.25;
+
+do {
+  contador++
+} while (!llueve())
+
+if (contador <= 1) {
+  console.log(`Fui a ver si llovía una vez.`);
+} else {
+  console.log(`Fui a ver si llovía ${contador} veces.`)
 }
 
-console.log(`Al inicio del año ${purpledoll.nombre} pesa ${purpledoll.peso}kg`);
-
-const INCREMENTO_PESO = 0.3;
-const DIAS_DEL_ANO = 365;
-
-const aumentarDePeso = persona => persona.peso += INCREMENTO_PESO;
-const adelgazar = persona => persona.peso -= INCREMENTO_PESO;
-const comeMucho = () => Math.random() < 0.3;
-const realizaDeporte = () => Math.random() < 0.4; 
-
-const META = purpledoll.peso - 3;
-var dias = 0;
-
-while (purpledoll.peso > META) {
-  if (comeMucho()) {
-    aumentarDePeso(purpledoll);
-  }
-  if (realizaDeporte()) {
-    adelgazar(purpledoll);
-  }
-  dias += 1;
-}
-
-console.log(`Pasaron ${dias} días hasta que ${purpledoll.nombre} adelgazó 3kg`);
