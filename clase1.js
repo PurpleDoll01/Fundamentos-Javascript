147b9009e03d7bd83eba37113b27716e11e8f289
Mariana Valencia <purple.mv@gmail.com>
2019-09-25 20:10:33 -0500

    Class

--- clase1.js
+++ clase1.js
@@ -1,62 +1,14 @@
-var sacha = {
-  nombre: 'Sacha',
-  apellido: 'Lifszyc',
-  altura: 1.72,
-  cantidadDeLibros: 111
+function Persona (nombre, apellido) {
+  this.nombre = nombre
+  this.apellido = apellido
 }
 
-var alan = {
-  nombre: 'Alan',
-  apellido: 'Perez',
-  altura: 1.86,
-  cantidadDeLibros: 78
+Persona.prototype.saludar = function () {
+  console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`);
 }
 
-var martin = {
-  nombre: 'Martin',
-  apellido: 'Gomez',
-  altura: 1.85,
-  cantidadDeLibros: 132
-}
+var purpledoll = new Persona('Mariana', 'Valencia');
+purpledoll.saludar();
 
-var dario = {
-  nombre: 'Dario',
-  apellido: 'Juarez',
-  altura: 1.71,
-  cantidadDeLibros: 90
-}
-
-var vicky = {
-  nombre: 'Vicky',
-  apellido: 'Zapata',
-  altura: 1.56,
-  cantidadDeLibros: 91
-}
-
-var paula = {
-  nombre: 'Paula',
-  apellido: 'Barros',
-  altura: 1.76,
-  cantidadDeLibros: 182
-}
-
-const esAlta = ({ altura }) => altura > 1.8;
-
-
-var personas = [sacha, alan, martin, dario, vicky, paula];
-
-var personasAltas = personas.filter(esAlta);
-
-const pasarAlturaACms = persona => ({
-  ...persona,
-    altura: persona.altura * 100
-})
-
-
-var personasCms = personas.map(pasarAlturaACms); 
-
-const reducer = (acum, { cantidadDeLibros}) => acum + cantidadDeLibros; 
-
-var totalDeLibros = personas.reduce(reducer, 0)
-
-console.log(`En total todos tienen ${totalDeLibros} libros`);
+var erika = new Persona ('Erika', 'Luna');
+erika.saludar();
