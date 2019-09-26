9052627b7313d3810584ffa4b4d661837708874e
Mariana Valencia <purple.mv@gmail.com>
2019-09-25 21:12:32 -0500

    Heritage old way

--- clase1.js
+++ clase1.js
@@ -1,3 +1,10 @@
+function heredaDe(prototipoHijo, prototipoPadre) {
+  var fn = function () {}
+  fn.prototype = prototipoPadre.prototype;
+  prototipoHijo.prototype = new fn;
+  prototipoHijo.prototype.constructor = prototipoHijo; 
+}
+
 function Persona (nombre, apellido, altura) {
   this.nombre = nombre
   this.apellido = apellido
@@ -9,10 +16,23 @@ Persona.prototype.saludar = function () {
 }
 
 Persona.prototype.soyAlto = function () {
-  debugger
   return this.altura > 1.8;
 }
+
+function Desarrollador(nombre, apellido) {
+  this.nombre = nombre
+  this.apellido = apellido
+}
+
+heredaDe(Desarrollador, Persona);
+
+Desarrollador.prototype.saludar = function () {
+  console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y soy desarrollador/a`);
+} 
+
+
  
 var purpledoll = new Persona('Mariana', 'Valencia', 1.54);
 var erika = new Persona ('Erika', 'Luna', 1.65);
 var arturo = new Persona ('Arturo', 'Martinez', 1.89);
+var arturito = new Desarrollador ('Arturo', 'Martinez', 1.89);
