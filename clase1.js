e3408e4fbef572b358cc649940905e39f3eba381
Mariana Valencia <purple.mv@gmail.com>
2019-09-25 21:44:48 -0500

    Class new way

--- clase1.js
+++ clase1.js
@@ -1,38 +1,32 @@
-function heredaDe(prototipoHijo, prototipoPadre) {
-  var fn = function () {}
-  fn.prototype = prototipoPadre.prototype;
-  prototipoHijo.prototype = new fn;
-  prototipoHijo.prototype.constructor = prototipoHijo; 
+class Persona {
+  constructor(nombre, apellido, altura) {
+    this.nombre = nombre
+    this.apellido = apellido
+    this.altura = altura
+  }
+
+  saludar() {
+    console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`);
+  }
+
+  soyAlto() {
+    return this.altura > 1.8;
+  }
 }
 
-function Persona (nombre, apellido, altura) {
-  this.nombre = nombre
-  this.apellido = apellido
-  this.altura = altura
+class Desarrollador extends Persona {
+  constructor(nombre, apellido, altura, pelo) {
+    super(nombre, apellido, altura)
+    this.pelo = pelo; 
+  }
+
+  saludar() {
+    console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y soy desarrollador/a`);
+  }
 }
-
-Persona.prototype.saludar = function () {
-  console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`);
-}
-
-Persona.prototype.soyAlto = function () {
-  return this.altura > 1.8;
-}
-
-function Desarrollador(nombre, apellido) {
-  this.nombre = nombre
-  this.apellido = apellido
-}
-
-heredaDe(Desarrollador, Persona);
-
-Desarrollador.prototype.saludar = function () {
-  console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y soy desarrollador/a`);
-} 
-
 
  
 var purpledoll = new Persona('Mariana', 'Valencia', 1.54);
 var erika = new Persona ('Erika', 'Luna', 1.65);
 var arturo = new Persona ('Arturo', 'Martinez', 1.89);
-var arturito = new Desarrollador ('Arturo', 'Martinez', 1.89);
+var arturito = new Desarrollador ('Arturo', 'Martinez', 1.89, 'rojo');
