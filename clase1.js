eb8a8da42c1380dfd99685ae28522394377929d0
Mariana Valencia <purple.mv@gmail.com>
2019-09-26 20:53:43 -0500

    Multiple requests

--- clase1.js
+++ clase1.js
@@ -1,9 +1,17 @@
 const API_URL = 'https://swapi.co/api/';
 const PEOPLE_URL = 'people/:id';
 
-const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id', 1)}`;
 const opts = { crossDomain: true };
 
-$.get(lukeUrl, opts, function (luke) {
-    console.log(`Hola, yo soy ${luke.name}`);
-});
+const onPeopleResponse = function (persona) {
+    console.log(`Hola, yo soy ${persona.name}`);
+}
+
+function obtenerPersonaje(id) {
+    const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`; 
+    $.get(url, opts, onPeopleResponse); 
+}
+
+obtenerPersonaje(1);
+obtenerPersonaje(2);
+obtenerPersonaje(3);
