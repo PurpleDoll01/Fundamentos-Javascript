3014f82e8c1cb90e785c5a876da61c1c868922bd
Mariana Valencia <purple.mv@gmail.com>
2019-09-26 22:24:12 -0500

    Promises .all

--- clase1.js
+++ clase1.js
@@ -18,19 +18,12 @@ function onError(id) {
     console.log(`Sucedió un error al obtener el personaje ${id}`) 
 }
 
-obtenerPersonaje(1)
-  .then(personaje => {
-    console.log(`El personaje 1 es ${personaje.name}`)  
-    return obtenerPersonaje(2);
-  }) 
-  .then(personaje => {
-    console.log(`El personaje 2 es ${personaje.name}`)  
-    return obtenerPersonaje(3) 
-  })
-  .then(personaje => {
-    console.log(`El personaje 3 es ${personaje.name}`)  
-  })
-  .catch(onError);
+var ids = [1, 2, 3, 4, 5, 6, 7];
+var promesas = ids.map(id => obtenerPersonaje(id))
+Promise
+    .all(promesas)
+    .then(personajes => console.log(personajes))
+    .catch(onError);
 
 
 
