0bff8f4e575cd66b5694ddefcdea800ea85d824f
Mariana Valencia <purple.mv@gmail.com>
2019-09-26 22:00:41 -0500

    Promises

--- clase1.js
+++ clase1.js
@@ -3,25 +3,26 @@ const PEOPLE_URL = 'people/:id';
 
 const opts = { crossDomain: true };
 
-function obtenerPersonaje(id, callback) {
-    const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`; 
-    
-    $
-    .get(url, opts, callback)
-    .fail(() => {
-        console.log(`Sucedió un error. No se pudo obtener el personaje ${id}`);
-    })
+function obtenerPersonaje(id) {
+    return new Promise((resolve, reject) => {
+        const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`;               
+        $
+         .get(url, opts, function(data) {
+            resolve(data)
+         })
+         .fail(() => reject(id))
+    })    
 }
 
-obtenerPersonaje(1, function(personaje) {
-    console.log(`Hola, yo soy ${personaje.name}`);
+function onError(id) {
+    console.log(`Sucedió un error al obtener el personaje ${id}`) 
+}
 
-    obtenerPersonaje(2, function(personaje) {
-        console.log(`Hola, yo soy ${personaje.name}`);
+obtenerPersonaje(1)
+  .then(function (personaje) {
+    console.log(`El personaje 1 es ${personaje.name}`)  
+  }) 
+  .catch(onError);
 
-        obtenerPersonaje(3, function(personaje) {
-            console.log(`Hola, yo soy ${personaje.name}`);
-        });
-    });
-});
+
 
